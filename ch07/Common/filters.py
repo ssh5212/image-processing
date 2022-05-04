b8c940977c99fb8f7488a3153d0e21b0725c2572
AngelPlayer <ssh5212@naver.com>
2022-05-04 12:16:11 +0900

    commit

--- ch07/Common/filters.py
+++ ch07/Common/filters.py
@@ -4,9 +4,11 @@ def differental(image, data1, data2):
     mask1 = np.array(data1, np.float32).reshape(3, 3)
     mask2 = np.array(data1, np.float32).reshape(3, 3)
 
-    dst1 = filter(image, mask1)
-    dst2 = filter(image, mask2)
-    dst = cv2.magnitude(dst1, dst2) # 두 행렬의 크기 계산 == 엣지 강도 계산
+    dst1 = cv2.filter2D(image, -1, mask1)
+    dst2 = cv2.filter2D(image, -1, mask2)
+    dst1, dst2 = np.abs(dst1), np.abs(dst2) # 절대값 계산 == 양수로 변경
+    # dst = cv2.magnitude(dst1, dst2) # 두 행렬의 크기 계산 == 엣지 강도 계산
+    dst = dst1 + dst2
 
     dst = cv2.convertScaleAbs(dst) # 절대값 및 형변환
     dst1 = cv2.convertScaleAbs(dst1) # 절대값 및 형변환
