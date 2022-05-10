8ae16473f24230d6321612d2fad7773b1ab72873
AngelPlayer <ssh5212@naver.com>
2022-05-10 15:25:15 +0900

    commit

--- ch08/Common/interpolation.py
+++ ch08/Common/interpolation.py
@@ -1,4 +1,5 @@
-import numpy as np, cv2
+import numpy as np, cv2, math
+from Common.functions import contain
 
 # 정방행렬 인덱스로 크기 변경
 def scaling(img, size):
@@ -33,4 +34,33 @@ def bilinear_value(img, pt):
     M1 = P1 + alpha * (P3 - P1) # 1차 보간
     M2 = P2 + alpha * (P4 - P2) 
     P = M1 + beta * (M2 - M1) # 2차 보간
+    
     return np.clip(P, 0, 255)
+
+def rotate_pt(img, degree, pt):
+    dst = np.zeros(img.shape[:2], img.dtype)
+    radian = (degree/180) * np.pi
+    sin, cos = math.sin(radian), math.cos(radian)
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            jj, ii = np.subtract((j, i), pt) # 중심좌표로 평행 이동
+
+            y = -jj*sin + ii*cos
+            x = jj*cos + ii*sin
+            x, y = np.add((x,y), pt)
+            if contain((y, x), img.shape):
+                dst[i, j] = bilinear_value(img, [x, y])
+    return dst
+
+
+# 어파인 변환 실행 함수
+def affine_transform(img, mat, size):
+    rows, cols = img.shape[:2]
+    inv_mat = cv2.invertAffineTransform(mat)
+
+    pts = [np.dot(inv_mat, (j, i, 1)) for i in range(rows) for j in range(cols)]
+    dst = [bilinear_value(img, p) if contain(p, size) else 0 for p in pts]
+    dst = np.reshape(dst, (rows, cols)).astype('uint8')
+
+    return dst
